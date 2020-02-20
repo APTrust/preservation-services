aa937ac6cd3c3b6443186abb69734378bb32a23e
A. Diamond <diamond@sheprador.com>
2020-02-20 11:51:45 -0500

    Starting Pharos integration tests

---
+++ network/pharos_client_int_test.go
@@ -0,0 +1,63 @@
+// +build integration
+
+package network_test
+
+import (
+	//"fmt"
+	// "github.com/APTrust/preservation-services/constants"
+	"github.com/APTrust/preservation-services/models/common"
+	"github.com/APTrust/preservation-services/network"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"net/url"
+	"testing"
+	//"time"
+)
+
+const InstIdentifier = "test.edu"
+
+var Institutions = []string{
+	"aptrust.org",
+	"institution1.edu",
+	"institution2.edu",
+	"test.edu",
+}
+
+func getPharosClient(t *testing.T) *network.PharosClient {
+	config := common.NewConfig()
+	assert.Equal(t, "test", config.ConfigName)
+	client, err := network.NewPharosClient(
+		config.PharosURL,
+		config.PharosAPIVersion,
+		config.PharosUser,
+		config.PharosAPIKey,
+	)
+	require.Nil(t, err)
+	require.NotNil(t, client)
+	return client
+}
+
+func TestPharosInstitutionGet(t *testing.T) {
+	client := getPharosClient(t)
+	resp := client.InstitutionGet(InstIdentifier)
+	assert.NotNil(t, resp)
+	assert.Nil(t, resp.Error)
+	institution := resp.Institution()
+	assert.NotNil(t, institution)
+	assert.Equal(t, InstIdentifier, institution.Identifier)
+}
+
+func TestPharosInstitutionList(t *testing.T) {
+	client := getPharosClient(t)
+	v := url.Values{}
+	v.Add("order", "name")
+	v.Add("per_page", "20")
+	resp := client.InstitutionList(v)
+	assert.NotNil(t, resp)
+	assert.Nil(t, resp.Error)
+	institutions := resp.Institutions()
+	assert.Equal(t, 4, len(institutions))
+	for i, inst := range institutions {
+		assert.Equal(t, Institutions[i], inst.Identifier)
+	}
+}
