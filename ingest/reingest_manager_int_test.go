a16932723c921816877c36ee815a7c9d7c01cadf
A. Diamond <diamond@sheprador.com>
2020-03-05 16:11:01 -0500

    A few more tests for ReingestManager

--- ingest/reingest_manager_int_test.go
+++ ingest/reingest_manager_int_test.go
@@ -3,11 +3,12 @@
 package ingest_test
 
 import (
-	//	"fmt"
-	//	"github.com/APTrust/preservation-services/constants"
+	"fmt"
+	"github.com/APTrust/preservation-services/constants"
 	"github.com/APTrust/preservation-services/ingest"
 	"github.com/APTrust/preservation-services/models/common"
-	//	"github.com/APTrust/preservation-services/models/service"
+	"github.com/APTrust/preservation-services/models/registry"
+	"github.com/APTrust/preservation-services/models/service"
 	//	"github.com/APTrust/preservation-services/util"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -61,4 +62,54 @@ func TestObjectWasPreviouslyIngested(t *testing.T) {
 	wasIngested, err = manager.ObjectWasPreviouslyIngested()
 	require.Nil(t, err)
 	assert.False(t, wasIngested)
+}
+
+func TestSetStorageOption(t *testing.T) {
+	genericFile := &registry.GenericFile{
+		State:         "D",
+		StorageOption: constants.StorageGlacierOH,
+	}
+	ingestFile := &service.IngestFile{
+		StorageOption: constants.StorageStandard,
+	}
+
+	// StorageOption should NOT change if GenericFile is deleted.
+	manager := GetReingestManager()
+	manager.SetStorageOption(ingestFile, genericFile)
+	assert.Equal(t, constants.StorageStandard, ingestFile.StorageOption)
+
+	// StorageOption should change if GenericFile is active.
+	genericFile.State = "A"
+	manager.SetStorageOption(ingestFile, genericFile)
+	assert.Equal(t, constants.StorageGlacierOH, ingestFile.StorageOption)
+}
+
+func TestFlagForUpdate(t *testing.T) {
+	pharosUUID := "c445c30b-2299-4796-b803-e3c6ee43a2ae"
+	genericFile := &registry.GenericFile{
+		URI: fmt.Sprintf("https://example.com/storage/%s", pharosUUID),
+	}
+	ingestFile := &service.IngestFile{
+		NeedsSave: false,
+		UUID:      "c4ddee73-cbae-4f4e-a93b-ffcb0a0f2e99",
+	}
+	manager := GetReingestManager()
+	manager.FlagForUpdate(ingestFile, genericFile)
+	assert.True(t, ingestFile.NeedsSave)
+	assert.Equal(t, pharosUUID, ingestFile.UUID)
+}
+
+func TestFlagUnchanged(t *testing.T) {
+	pharosUUID := "c445c30b-2299-4796-b803-e3c6ee43a2ae"
+	genericFile := &registry.GenericFile{
+		URI: fmt.Sprintf("https://example.com/storage/%s", pharosUUID),
+	}
+	ingestFile := &service.IngestFile{
+		NeedsSave: true,
+		UUID:      "c4ddee73-cbae-4f4e-a93b-ffcb0a0f2e99",
+	}
+	manager := GetReingestManager()
+	manager.FlagUnchanged(ingestFile, genericFile)
+	assert.False(t, ingestFile.NeedsSave)
+	assert.Equal(t, pharosUUID, ingestFile.UUID)
 }
