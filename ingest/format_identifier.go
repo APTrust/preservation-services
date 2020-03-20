ece03d6a6d4b427bfc3bbb8860d77b07fd6043e0
A. Diamond <diamond@sheprador.com>
2020-03-20 16:14:02 -0400

    Code cleanup and notes

--- ingest/format_identifier.go
+++ ingest/format_identifier.go
@@ -2,15 +2,9 @@ package ingest
 
 import (
 	"fmt"
-	// "github.com/APTrust/preservation-services/constants"
 	"github.com/APTrust/preservation-services/models/common"
 	"github.com/APTrust/preservation-services/models/service"
 	"github.com/APTrust/preservation-services/util"
-	// "github.com/minio/minio-go/v6"
-	// "io"
-	// "os/exec"
-	"strings"
-	// "time"
 )
 
 // FormatIdentifier streams an S3 file, or the first chunk of it, through
@@ -18,73 +12,46 @@ import (
 // is FIDO, which uses the PRONOM registry to identify formats.
 type FormatIdentifier struct {
 	Context      *common.Context
+	Identifier   *util.FormatIdentifier
 	IngestObject *service.IngestObject
 	WorkItemId   int
-	CurlCmd      string
-	PythonCmd    string
 }
 
-// NewFormatIdentifier creates a new FormatIdentifier.
+// NewFormatIdentifier creates a new FormatIdentifier. This will panic
+// if the prerequisites for running the format identifier script are
+// not present.
 func NewFormatIdentifier(context *common.Context, workItemId int, ingestObject *service.IngestObject) *FormatIdentifier {
-	f := &FormatIdentifier{
+	pathToScript := context.Config.FormatIdentifierScript()
+	identifier := util.NewFormatIdentifier(pathToScript)
+	if !identifier.CanRun() {
+		panic(fmt.Sprintf("Missing prerequisites for format identifier. "+
+			"Be sure the following are installed: curl, fido, python2, and "+
+			"identify_format.sh. The last should be at %s", pathToScript))
+	}
+	return &FormatIdentifier{
 		Context:      context,
+		Identifier:   identifier,
 		IngestObject: ingestObject,
 		WorkItemId:   workItemId,
 	}
-	// Do this once to avoid unnecessary exec calls.
-	curlCmd, pythonCmd := f.GetBaseCommands()
-	f.CurlCmd = curlCmd
-	f.PythonCmd = pythonCmd
-
-	return f
 }
 
-// GetCommand returns a command for streaming S3 data through FIDO.
-// This performs some rudimentary checks of the URL and filename to
-// ensure we're not passing unsafe data to an external command. Since
-// URLs come from pharos and follow a known format (s3host/bucket/uuid)
-// and because we're constructing the filenames ourselves (uuid.ext),
-// we should not have problems.
-func (f *FormatIdentifier) GetCommandString(url, filename string) (string, error) {
-	unsafeChars := "';{}|$"
-	if strings.ContainsAny(url, unsafeChars) {
-		return "", fmt.Errorf("URL contains unsafe characters")
-	}
-	if strings.ContainsAny(filename, unsafeChars) {
-		return "", fmt.Errorf("File name contains unsafe characters")
-	}
-	cmdString := fmt.Sprintf("%s '%s' | %s -", f.CurlCmd, url, f.PythonCmd)
-	if filename != "" {
-		cmdString = fmt.Sprintf("%s '%s' | %s -filename='%s' -", f.CurlCmd, url, f.PythonCmd, filename)
-	}
-	return cmdString, nil
-}
-
-// GetBaseCommands returns the basic commands we'll need to run S3 data
-// through FIDO. We should only call this one, when the FormatIdentifier
-// object is constructed, so we don't wind up calling `which` three times
-// for every file.
-func (f *FormatIdentifier) GetBaseCommands() (string, string) {
-	fido := f.PathTo("fido")
-	python2 := f.PathTo("python2")
-	curl := f.PathTo("curl")
-	matchFormat := "OK,%(info.mimetype)s,%(info.matchtype)s\n"
-	noMatchFormat := "FAIL,,%(info.matchtype)s\n"
-
-	curlCmd := fmt.Sprintf("%s -s -r 0-524288 ", curl)
-	pythonCmd := fmt.Sprintf(`%s -u %s -q -matchprintf="%s" -nomatchprintf="%s" -nocontainer`, python2, fido, matchFormat, noMatchFormat)
-
-	return curlCmd, pythonCmd
-}
-
-// PathTo returns the path to the specified program, as returned by `which`.
-// Because the FormatIdentifier cannot proceed if any of the required
-// programs are missing (curl, python2, and fido), this call panics if the
-// requested program cannot be found in the system's PATH.
-func (f *FormatIdentifier) PathTo(program string) string {
-	pathToProgram, err := util.PathTo(program)
-	if err != nil || pathToProgram == "" {
-		panic(fmt.Sprintf("Can't find %s in your PATH", program))
-	}
-	return pathToProgram
-}
+// TODO:
+//
+// For each IngestFile:
+//
+// Get Redis record
+//   Skip if it already has a format identification timestamp and method
+// Get signed URL for item in staging
+// Run identifier on signed URL
+//   If identification completes and fails, stick with the existing mime type
+//   and note that ident tried and failed.
+//   If identification cannot complete due to bad server response, proceed
+//   to the next file.
+//   Track whether all files complete & if any need retries.
+// If format changed:
+//   Save format back to Redis record
+//   Use CopyObject to update ContentType metadata on S3 object
+// Stamp Redis record with new ContentType, time of identification and method
+//   (Method comes from identifier script: 'signature' or 'extension')
+// Save Redis record
--- ingest/format_identifier_int_test.go
+++ ingest/format_identifier_int_test.go
@@ -3,15 +3,10 @@
 package ingest_test
 
 import (
-	//"github.com/APTrust/preservation-services/bagit"
-	//"github.com/APTrust/preservation-services/constants"
 	"github.com/APTrust/preservation-services/ingest"
 	"github.com/APTrust/preservation-services/models/common"
-	//"github.com/APTrust/preservation-services/util/testutil"
-	//"github.com/minio/minio-go/v6"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	//"path"
 	"testing"
 )
 
@@ -23,33 +18,10 @@ func getFormatIdentifier(t *testing.T) *ingest.FormatIdentifier {
 	assert.Equal(t, context, f.Context)
 	assert.Equal(t, obj, f.IngestObject)
 	assert.Equal(t, suWorkItemId, f.WorkItemId)
-	assert.NotEqual(t, "", f.CurlCmd)
-	assert.NotEqual(t, "", f.PythonCmd)
+	assert.NotNil(t, f.Identifier)
 	return f
 }
 
 func TestNewFormatIdentifier(t *testing.T) {
 	getFormatIdentifier(t)
-}
-
-func TestPathTo(t *testing.T) {
-	f := getFormatIdentifier(t)
-	assert.NotPanics(t, func() {
-		f.PathTo("ls")
-	}, "PathTo with legit program should not panic")
-
-	path := f.PathTo("ls")
-	assert.NotEqual(t, "", path)
-
-	assert.Panics(t, func() {
-		f.PathTo("xxxxyyyyzzzz")
-	}, "PathTo with non-existent program should panic")
-}
-
-func TestGetBaseCommands(t *testing.T) {
-
-}
-
-func TestGetCommandString(t *testing.T) {
-
 }
