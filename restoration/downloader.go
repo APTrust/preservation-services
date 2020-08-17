999009bde43645a4f9769a876c2f49e951f4509f
A. Diamond <diamond@sheprador.com>
2020-08-17 16:16:04 -0400

    Added skeleton for restoration downloader

--- restoration/downloader.go
+++ restoration/downloader.go
@@ -1,5 +1,31 @@
 package restoration
 
+import (
+	"github.com/APTrust/preservation-services/models/common"
+	"github.com/APTrust/preservation-services/models/service"
+)
+
 // Downloader downloads all files required to restore an IntellectualObject.
 type Downloader struct {
+	Base
+}
+
+// NewDownloader creates a new Downloader to copy files from S3
+// to local disk for packaging.
+func NewDownloader(context *common.Context, workItemID int, restorationObject *service.RestorationObject) *Downloader {
+	return &Downloader{
+		Base: Base{
+			Context:           context,
+			RestorationObject: restorationObject,
+			WorkItemID:        workItemID,
+		},
+	}
+}
+
+func (d *Downloader) Run() (fileCount int, errors []*service.ProcessingError) {
+	// Loop:
+	// Get batch of files from Pharos
+	// Download each file
+	// Mark RestorationObject.AllFilesDownloaded to true
+	return fileCount, errors
 }
