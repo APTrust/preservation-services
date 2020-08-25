389e5d60ba68d8f947e2f9f4554dbc7a2c3e40ae
A. Diamond <diamond@sheprador.com>
2020-08-25 15:26:41 -0400

    Started BagRestorer integration tests

--- restoration/bag_restorer_int_test.go
+++ restoration/bag_restorer_int_test.go
@@ -5,13 +5,44 @@ package restoration_test
 import (
 	"testing"
 
+	"github.com/APTrust/preservation-services/constants"
+	"github.com/APTrust/preservation-services/models/common"
+	"github.com/APTrust/preservation-services/models/service"
 	"github.com/APTrust/preservation-services/restoration"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewBagRestorer(t *testing.T) {
+// TODO: Mock Pharos and S3 here? Or do full integration test?
+// The second option will require ingesting a bag.
+//  - Copy bag to receiving bucket
+//  - Create WorkItem (using bucket reader "run once"?)
+//  - Poll for completion of ingest
+//  - Run tests
 
+const workItemID = 9999
+
+func getRestorationObject() *service.RestorationObject {
+	return &service.RestorationObject{
+		Identifier:             "test.edu/apt-bag-1",
+		BagItProfileIdentifier: constants.DefaultProfileIdentifier,
+		FileSize:               6500000,
+		RestorationSource:      constants.RestorationSourceS3,
+		RestorationTarget:      "aptrust.restore.test.test.edu",
+		RestorationType:        constants.RestorationTypeObject,
+	}
+}
+
+func getRestorer() *restoration.BagRestorer {
+	return restoration.NewBagRestorer(common.NewContext(), workItemID, getRestorationObject())
+}
+
+func TestNewBagRestorer(t *testing.T) {
+	r := getRestorer()
+	require.NotNil(t, r)
+	require.NotNil(t, r.Context)
+	assert.Equal(t, workItemID, r.WorkItemID)
+	assert.Equal(t, "test.edu/apt-bag-1", r.RestorationObject.Identifier)
 }
 
 func TestBagRestorer_RecordDigests(t *testing.T) {
