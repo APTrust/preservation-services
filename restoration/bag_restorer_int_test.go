98e39b3156537a9f82bf9034687dd06a1d9dac81
A. Diamond <diamond@sheprador.com>
2020-08-26 14:42:29 -0400

    Starting bag restoration integration tests

---
+++ restoration/bag_restorer_int_test.go
@@ -0,0 +1,152 @@
+// +build integration
+
+package restoration_test
+
+import (
+	"io/ioutil"
+	"path"
+	"testing"
+
+	"github.com/APTrust/preservation-services/constants"
+	"github.com/APTrust/preservation-services/models/common"
+	"github.com/APTrust/preservation-services/models/service"
+	"github.com/APTrust/preservation-services/restoration"
+	"github.com/APTrust/preservation-services/util/testutil"
+	"github.com/minio/minio-go/v6"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var bagRestorerSetupCompleted = false
+
+const workItemID = 334455
+
+type RestorationItem struct {
+	WorkItemID    int
+	ObjIdentifier string
+	BagItProfile  string
+}
+
+var itemsToRestore = []RestorationItem{
+	RestorationItem{
+		WorkItemID:    87777,
+		ObjIdentifier: "test.edu/apt-test-restore",
+		BagItProfile:  constants.BagItProfileDefault,
+	},
+	RestorationItem{
+		WorkItemID:    87999,
+		ObjIdentifier: "test.edu/btr-512-test-restore",
+		BagItProfile:  constants.BagItProfileBTR,
+	},
+}
+
+// setup ensures the files we want to restore are in the local Minio
+// preservation buckets. All other info pertaining to these files/bags
+// is loaded from fixture data into Pharos by the test script in
+// scripts/test.rb
+func setup(t *testing.T, context *common.Context) {
+	if bagRestorerSetupCompleted {
+		return
+	}
+
+	s3Client := context.S3Clients[constants.StorageProviderAWS]
+
+	// Our test files should be in these two preservation buckets,
+	// according to the Pharos fixture data.
+	preservationBuckets := []string{
+		context.Config.BucketStandardVA,
+		context.Config.BucketStandardOR,
+	}
+
+	// Copy the files from int_test_bags/restoration/files to the
+	// local Minio preservation buckets.
+	dir := path.Join(testutil.PathToTestData(), "int_test_bags", "restoration", "files")
+	files, err := ioutil.ReadDir(dir)
+	require.Nil(t, err)
+	for _, file := range files {
+		fullpath := path.Join(dir, file.Name())
+		for _, bucket := range preservationBuckets {
+			_, err := s3Client.FPutObject(
+				bucket,
+				file.Name(),
+				fullpath,
+				minio.PutObjectOptions{})
+			require.Nil(t, err)
+		}
+	}
+	bagRestorerSetupCompleted = true
+}
+
+func getRestorationObject(objIdentifier string) *service.RestorationObject {
+	return &service.RestorationObject{
+		Identifier:             objIdentifier,
+		BagItProfileIdentifier: constants.DefaultProfileIdentifier,
+		RestorationSource:      constants.RestorationSourceS3,
+		RestorationTarget:      "aptrust.restore.test.test.edu",
+		RestorationType:        constants.RestorationTypeObject,
+	}
+}
+
+func TestNewBagRestorer(t *testing.T) {
+	item := itemsToRestore[0]
+	restorer := restoration.NewBagRestorer(
+		common.NewContext(),
+		item.WorkItemID,
+		getRestorationObject(item.ObjIdentifier))
+	require.NotNil(t, restorer)
+	require.NotNil(t, restorer.Context)
+	assert.Equal(t, item.WorkItemID, restorer.WorkItemID)
+	assert.Equal(t, item.ObjIdentifier, restorer.RestorationObject.Identifier)
+}
+
+func TestBagRestorer_Run(t *testing.T) {
+	context := common.NewContext()
+	setup(t, context)
+	for _, item := range itemsToRestore {
+		restObj := getRestorationObject(item.ObjIdentifier)
+		restorer := restoration.NewBagRestorer(context, item.WorkItemID, restObj)
+		fileCount, errors := restorer.Run()
+		assert.True(t, fileCount >= 3)
+		assert.Empty(t, errors)
+	}
+}
+
+// func TestBagRestorer_RecordDigests(t *testing.T) {
+
+// }
+
+// func TestBagRestorer_AppendDigestToManifest(t *testing.T) {
+
+// }
+
+// func TestBagRestorer_GetManifestPath(t *testing.T) {
+
+// }
+
+// func TestBagRestorer_DeleteStaleManifests(t *testing.T) {
+
+// }
+
+// func TestBagRestorer_BestRestorationSource(t *testing.T) {
+
+// }
+
+// func TestBagRestorer_GetBatchOfFiles(t *testing.T) {
+
+// }
+
+// func TestBagRestorer_GetTarHeader(t *testing.T) {
+
+// }
+
+// func TestBagRestorer_AddBagItFile(t *testing.T) {
+
+// }
+
+// func TestBagRestorer_AddManifests(t *testing.T) {
+
+// }
+
+// func TestBagRestorer_AddToTarFile(t *testing.T) {
+
+// }
--- testdata/int_test_bags/README.md
+++ testdata/int_test_bags/README.md
@@ -12,3 +12,10 @@ updated bags contain mostly the same files, with these differences:
 * data/files/data.xml  -> changed
 * data/files/.DS_Store -> added
 * data/files/file_example_SVG_20kB.svg -> added
+
+The "restoration" folder contains bags for restoration tests in
+restoration/bag\_restorer\_int\_test.go. It also contains files to be restored
+in the files directory. During integration tests, those files, whose names
+match the UUIDs in Pharos fixture data, are copied into local Minio
+preservation buckets by bag\_restorer\_int\_test.go so the restoration tests
+can proceed.
