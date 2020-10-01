e0a5db97f54ab21cfd35145ed7c0e0a4a076d72e
A. Diamond <diamond@sheprador.com>
2020-10-01 15:23:21 -0400

    Start e2e cleanup and refactor

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -42,6 +42,7 @@ func TestEndToEnd(t *testing.T) {
 	// and premis events from these ingests are as expected
 	testPharosObjects()
 	testGenericFiles()
+
 }
 
 // Set up a context for testing.
@@ -137,42 +138,6 @@ func ingestsComplete(count uint64) bool {
 	require.Nil(ctx.T, err)
 	ctx.Context.Logger.Infof("In %s: %d in flight, %d finished. Want %d", channelName, summary.InFlightCount, summary.FinishCount, count)
 	return summary.InFlightCount == 0 && summary.FinishCount == count
-}
-
-// This is the meat of the ingest test: Make sure that all
-// expected objects, files, etc. are in Pharos.
-func testPharosObjects() {
-	for _, expectedObj := range ctx.ExpectedObjects {
-		testObject(expectedObj)
-	}
-}
-
-func testObject(expectedObj *registry.IntellectualObject) {
-	resp := ctx.Context.PharosClient.IntellectualObjectGet(expectedObj.Identifier)
-	require.Nil(ctx.T, resp.Error)
-	pharosObj := resp.IntellectualObject()
-	require.NotNil(ctx.T, pharosObj, "Pharos is missing %s", expectedObj.Identifier)
-	testObjAgainstExpected(pharosObj, expectedObj)
-}
-
-func testObjAgainstExpected(pharosObj, expectedObj *registry.IntellectualObject) {
-	t := ctx.T
-	assert.Equal(t, pharosObj.Title, expectedObj.Title, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.Description, expectedObj.Description, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.Identifier, expectedObj.Identifier, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.AltIdentifier, expectedObj.AltIdentifier, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.Access, expectedObj.Access, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.BagName, expectedObj.BagName, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.State, expectedObj.State, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.BagGroupIdentifier, expectedObj.BagGroupIdentifier, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.StorageOption, expectedObj.StorageOption, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.BagItProfileIdentifier, expectedObj.BagItProfileIdentifier, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.SourceOrganization, expectedObj.SourceOrganization, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.InternalSenderIdentifier, expectedObj.InternalSenderIdentifier, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.InternalSenderDescription, expectedObj.InternalSenderDescription, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.FileCount, expectedObj.FileCount, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.FileSize, expectedObj.FileSize, expectedObj.Identifier)
-	assert.Equal(t, pharosObj.Institution, expectedObj.Institution, expectedObj.Identifier)
 }
 
 func testWorkItemsAfterIngest() {
