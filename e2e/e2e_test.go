7a4047a04166a1759bab69cc72d4d5b587d4eba1
A. Diamond <diamond@sheprador.com>
2020-10-01 14:53:39 -0400

    PremisEvent tests are passing

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -3,7 +3,7 @@
 package e2e_test
 
 import (
-	//"fmt"
+	"fmt"
 	"net/url"
 	"strconv"
 	"strings"
@@ -314,9 +314,33 @@ func testStorageRecords(pharosFile, expectedFile *registry.GenericFile) {
 }
 
 func testPremisEvents(pharosFile, expectedFile *registry.GenericFile) {
+	assert.Equal(ctx.T, len(expectedFile.PremisEvents), len(pharosFile.PremisEvents))
+	pharosEvents := hashEvents(pharosFile.PremisEvents)
+	for _, event := range expectedFile.PremisEvents {
+		key := eventKey(event)
+		pharosEvent := pharosEvents[key]
+		assert.NotNil(ctx.T, pharosEvent, "Pharos file %s is missing event %s", pharosFile.Identifier, key)
+	}
+}
 
-	// START HERE...
+// Use hash/map instead of repeated nested loop lookups
+func hashEvents(events []*registry.PremisEvent) map[string]*registry.PremisEvent {
+	eventMap := make(map[string]*registry.PremisEvent)
+	for _, e := range events {
+		eventMap[eventKey(e)] = e
+	}
+	return eventMap
+}
 
+// Unique key to match expected and actual events.
+// Key must include type and outcome info, and must not include
+// UUIDs that change on every ingest.
+func eventKey(event *registry.PremisEvent) string {
+	suffix := event.OutcomeDetail
+	if event.EventType == constants.EventIdentifierAssignment || event.EventType == constants.EventReplication {
+		suffix = event.OutcomeInformation
+	}
+	return fmt.Sprintf("%s / %s", event.EventType, suffix)
 }
 
 func GetInstitution(identifier string) *registry.Institution {
