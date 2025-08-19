663faf8030d26e4a810dd89f03946838ea42ab68
Melissa Iori <melissa.iori@aptrust.org>
2025-08-18 22:39:51 -0400

    Removed unneccesary restoration bit

--- restoration/file_restorer.go
+++ restoration/file_restorer.go
@@ -67,10 +67,6 @@ func (r *FileRestorer) getGenericFile() (*registry.GenericFile, error) {
 		return nil, resp.Error
 	}
 	gf := resp.GenericFile()
-
-	// Replace URL encoded characters with the original characters.
-	// gf.Path = strings.Replace(gf.Path, "%E2%80%AF", " ", -1)
-
 	if gf == nil {
 		return nil, fmt.Errorf("Registry returned nil for file %s", r.RestorationObject.Identifier)
 	}
